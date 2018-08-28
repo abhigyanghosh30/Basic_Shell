672839aa3e44297658bc8fb1856365cd25173c10
abhigyanghosh30 <abhigyanghosh30@gmail.com>
2018-08-28 15:42:30 +0100

    replace depricated getcwd with getenv

--- assignment2.c
+++ assignment2.c
@@ -16,14 +16,37 @@ struct command
     char *argv[MAXARGS];    //pointer to list of arguments
     enum builtin_t
     {
-        NONE, QUIT, JOBS, BG, FG
+        QUIT, JOBS, BG, FG
     } builtin; 
 };
 
 //global variables
 struct command cmd;
 char *cmdline;
-char prompt[] = "lnsh>";
+char prompt[MAXLINE];
+
+void strip() //similar to the strip function in Python
+{
+    for(int i=0;i<cmd.argc;i++)
+    {
+        if(cmd.argv[i]==NULL)
+            break;
+        int len=strlen(cmd.argv[i]);
+        printf("%d\t",len);
+        for(int j=len-1;j>=0;j--)
+        {
+            if(cmd.argv[i][j] == ' ')
+                cmd.argv[i][j]='\0';
+            else   
+                break;
+        }
+    }
+}
+
+int shell_ls()
+{
+    ;;
+}
 
 void runBuiltinCommand(){}
 void runSystemCommand(struct command *cmd, int bg)
@@ -57,11 +80,11 @@ int parse()
         return 0;
     }
     
-    cmd.argv[0]= strtok(cmdline,"-");
+    cmd.argv[0]= strtok(cmdline,"- ");
     for(int i=0;cmd.argv[i]!=NULL;i++)
     {
         cmd.argc++;     //number of arguments including the command itself
-        cmd.argv[i+1] = strtok(0,"-");
+        cmd.argv[i+1] = strtok(0,"- ");
         printf("%s\t",cmd.argv[i+1]);
     }
     printf("%d",cmd.argc);
@@ -74,52 +97,6 @@ int parse()
 
 }
 
-// int parse(char *cmdline, struct command *cmd)
-// {
-//     char array[MAXLINE]; //local copy of the command line
-//     char delims[10]=" \t\r\n";
-//     char *line = array;         //pointer that traverses the commandline
-//     char *token;                //pointer to the end of the current argument
-//     char *endline;              //pointer to the end of the commandline string
-//     int is_bg;                  //to check if it's a background job
-
-//     if(cmdline == NULL)
-//         // error("command line is NULL\n");
-    
-//     (void)strncpy(line,cmdline,MAXLINE);
-//     endline = line + strlen(line);
-//     cmd->argc = 0;
-//     while (line < endline)
-//     {
-//         line += strspn(line, delims);
-//         if(line > endline)
-//             break;
-        
-//         token = line + strcspn(line, delims);
-//         *token = '\0';
-
-//         cmd->argv[cmd->argc++] = line;
-
-//         if(cmd->argc >= MAXARGS - 1)
-//             break;
-        
-//         line = token + 1;
-
-//     }
-//     cmd->argv[cmd->argc] = NULL;
-
-//     if(cmd->argc == 0)
-//         return 1;
-//     for(int i=0;i<cmd->argc;i++)
-//         printf("%s",cmd->argv[i]);
-//     //cmd->builtin = parseBuiltin(cmd);
-
-//     if((is_bg = (*cmd->argv[cmd->argc - 1] == '&')) != 0)
-//         cmd->argv[--cmd->argc] = NULL;
-
-//     return is_bg;
-// }
-
 
 void eval()
 {
@@ -130,6 +107,8 @@ void eval()
     // parse the line to fit values into cmd structure
     bg = parse();
 
+    strip();    // strips off trailing whitespaces
+
     //if errors in parsing
     if(bg == 1)
         return;
@@ -137,16 +116,24 @@ void eval()
     if(cmd.argv[0]==NULL)   
         return;
 
-    if(cmd.builtin == NONE)
-        runSystemCommand(&cmd, bg);
-    else
-        runBuiltinCommand(&cmd, bg);
-
+    if(!strcmp(cmd.argv[0],"ls"))
+    {
+        printf("Evaluating list\n");
+        shell_ls();
+    }
+    else if(!strcmp(cmd.argv[0],"cd"))
+    {
+        printf("Evaluating change dir\n");
+    }
 }
 
 int main(int argc, char **argv)
 {
-    
+    strcpy(prompt,getenv("HOME"));
+    int length_of_prompt = strlen(prompt);
+    prompt[length_of_prompt] = '>';
+    prompt[++length_of_prompt] = '\0';
+
     cmdline = malloc(MAXLINE);
     if (cmdline == NULL) {
         printf("No memory\n");
@@ -156,7 +143,7 @@ int main(int argc, char **argv)
     while(1)
     {
         printf("%s",prompt);
-
+        cmd.argc = 0;
         if((fgets(cmdline,MAXLINE,stdin)==NULL) && ferror(stdin))
             printf("fgets error");
         
